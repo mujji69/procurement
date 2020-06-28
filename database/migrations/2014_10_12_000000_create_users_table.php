719c83387abba1f15b8790e557b302d1144e65ae
muzammil <muzammil.asrar98@gmail.com>
2020-06-29 00:30:42 +0500

    updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+
             $table->string('org');
             $table->string('address');
             $table->string('contact');
