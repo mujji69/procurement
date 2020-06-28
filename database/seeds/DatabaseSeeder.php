72719518d88b234515605e64a527d92b7af8324c
muzammil <muzammil.asrar98@gmail.com>
2020-06-29 00:47:24 +0500

    updated

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('admins')->insert([
+           'username' => 'admin',
+           'password' => Hash::make('admin'),
+       ]);
     }
 }
